c9d38a6e3af7423da7ba6a8983729deefdc06765
amjadsh92 <amjadsharafeddien@gmail.com>
2024-03-23 22:03:47 +0100

    Figuring how to create an POST API route

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,20 +1,37 @@
-
 import { NextResponse } from "next/server";
 import pool from "../../../lib/mysql";
 
 export async function GET() {
-    try {
-        const db = await pool.getConnection()
-        if (db) console.log("It is connected")
-        else console.log("not connected")
-        const query = 'SELECT * FROM products'
-        const [rows] = await db.execute(query)
-        db.release()
-        
-    return NextResponse.json(rows)
-} catch (error) {
-    return NextResponse.json({
-        error: error
-    }, { status: 500 })
+  try {
+    const db = await pool.getConnection();
+    const query = "SELECT * FROM products";
+    const [rows] = await db.execute(query);
+    db.release();return NextResponse.json(rows);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error,
+      },
+      { status: 500 }
+    );
+  }
 }
+
+export async function POST(request) {
+  try {
+    const data = await request.json();
+    const db = await pool.getConnection();
+    const { name, description } = data;
+    const query = `INSERT INTO products (name, description) VALUES ('${name}', '${description}')`;
+    const [results] = await db.execute(query);
+    db.release();
+    return NextResponse.json(results);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error,
+      },
+      { status: 500 }
+    );
+  }
 }
