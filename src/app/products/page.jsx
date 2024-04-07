06810ccb4f4268aad2715cc2860a34434b45ef90
amjadsh92 <amjadsharafeddien@gmail.com>
2024-04-07 12:00:30 +0200

    Adding and deleting products + using useSeletor to access the store directly

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,53 +2,96 @@
 
 import { useEffect, useState } from "react";
 import Styles from "./products.module.css";
-import axios from "axios";
-import  { useSelector, useDispatch }  from  'react-redux'  
-import  { selectAllProducts, fetchProducts } from '../../lib/features/products/productsSlice'  
-
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchProducts,
+  AddNewProducts,
+  deleteProducts,
+} from "../../lib/features/products/productsSlice";
 
 export default function Products() {
-  
+  const dispatch = useDispatch();
+  const productStatus = useSelector((state) => state.prod.status);
 
-  const dispatch =  useDispatch()  
-	const productStatus =  useSelector(state  => state.prod.status)  
-	useEffect(()  =>  {  
-	if  (productStatus ===  'idle')  {  
-	dispatch(fetchProducts())  
-	}  
-	},  [productStatus, dispatch])  
-  
-  const products =  useSelector(selectAllProducts()) 
-  
- console.log('productsclient', products)
-  
+  useEffect(() => {
+    if (productStatus === "idle") {
+      dispatch(fetchProducts());
+    }
+  }, [productStatus, dispatch]);
 
   return (
     <div className={Styles.main}>
-    
-      <ShowProducts products={products} />
-     
+      <SubmitProducts />
+      <ShowProducts />
     </div>
   );
 }
 
-function ShowProducts({ products }) {
+function SubmitProducts() {
+  const [productForSubmit, setProductForSubmit] = useState({
+    name: "",
+    description: "",
+  });
+
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setProductForSubmit({
+      ...productForSubmit,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  function handleClick() {
+    dispatch(AddNewProducts(productForSubmit));
+    setProductForSubmit({ name: "", description: "" });
+  }
+
+  return (
+    <>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={productForSubmit.name}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        description:
+        <input
+          type="text"
+          name="description"
+          value={productForSubmit.description}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit" onClick={handleClick}>
+        Add Product
+      </button>
+    </>
+  );
+}
+
+function ShowProducts() {
+  const products = useSelector((state) => state.prod.products);
   return (
     <div>
       {products
         ? products.map((product) => (
-            <RenderShowProducts
-              product={product}
-              
-              key={product.id}
-            />
+            <RenderShowProducts product={product} key={product.id} />
           ))
         : console.log("undefined")}
     </div>
   );
 }
 
-function RenderShowProducts({ product}) {
+function RenderShowProducts({ product }) {
+  const dispatch = useDispatch();
+
   return (
     <>
       <ol>
@@ -56,8 +99,13 @@ function RenderShowProducts({ product}) {
         <li>{product.name}</li>
         <li>{product.description}</li>
       </ol>
-      
+      <button
+        type="submit"
+        value="Delete"
+        onClick={() => dispatch(deleteProducts(product.id))}
+      >
+        Delete product
+      </button>
     </>
   );
 }
-
